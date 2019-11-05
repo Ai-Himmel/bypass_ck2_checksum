3f4f2129e891a85bdf40e25bf2e069629d53f65a
AiHimmel <AiHimmel@msn.com>
2019-11-05 16:32:41 +0800

    添加项目文件。

---
+++ bypass_ck2_checksum/PatternSearch.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+
+#define REBASE(pRVA, baseOld, baseNew)       ((ptr_t)pRVA - (ptr_t)baseOld + (ptr_t)baseNew)
+
+
+
+#include <string>
+#include <vector>
+#include <initializer_list>
+using ptr_t = uint64_t;
+
+
+	class PatternSearch
+	{
+	public:
+		 PatternSearch(const std::vector<uint8_t>& pattern);
+		 PatternSearch(const std::initializer_list<uint8_t>&& pattern);
+		 PatternSearch(const std::string& pattern);
+		 PatternSearch(const char* pattern, size_t len = 0);
+		 PatternSearch(const uint8_t* pattern, size_t len = 0);
+
+		 ~PatternSearch() = default;
+
+		/// <summary>
+		/// Default pattern matching with wildcards.
+		/// std::search is approximately 2x faster than naive approach.
+		/// </summary>
+		/// <param name="wildcard">Pattern wildcard</param>
+		/// <param name="scanStart">Starting address</param>
+		/// <param name="scanSize">Size of region to scan</param>
+		/// <param name="out">Found results</param>
+		/// <param name="value_offset">Value that will be added to resulting addresses</param>
+		/// <returns>Number of found addresses</returns>
+		 size_t Search(
+			uint8_t wildcard,
+			void* scanStart,
+			size_t scanSize,
+			std::vector<ptr_t>& out,
+			ptr_t value_offset = 0
+		) const;
+
+		/// <summary>
+		/// Full pattern match, no wildcards.
+		/// Uses Boyer�Moore�Horspool algorithm.
+		/// </summary>
+		/// <param name="scanStart">Starting address</param>
+		/// <param name="scanSize">Size of region to scan</param>
+		/// <param name="out">Found results</param>
+		/// <param name="value_offset">Value that will be added to resulting addresses</param>
+		/// <returns>Number of found addresses</returns>
+		 size_t Search(
+			void* scanStart,
+			size_t scanSize,
+			std::vector<ptr_t>& out,
+			ptr_t value_offset = 0
+		) const;
+	private:
+		std::vector<uint8_t> _pattern;      // Pattern to search
+	};
+
---
+++ bypass_ck2_checksum/dllmain.cpp
@@ -0,0 +1,51 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+
+#include <string>
+#include "PatternSearch.h"
+#include <windows.h>
+#include <psapi.h>
+
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+	
+	
+    switch (ul_reason_for_call)
+    {
+	case DLL_PROCESS_ATTACH: {
+		/*
+		mov al,1
+		nop		
+		*/
+		char dummy[] = { 0xB0,0x01,0x90 };
+
+		/*
+		mov eax,[ebp+?]
+		setz al
+		mov eax,[ebp+?]
+		*/
+		PatternSearch ps{ 0x88,0x47,0x00,0x0F,0x94,0xC0,0x88,0x47,0x00 };//0x00 is wildcard
+		std::vector<ptr_t> ptr_t;
+		HANDLE hP = GetCurrentProcess();
+		HMODULE hM = GetModuleHandleA("CK2game.exe");
+		MODULEINFO mod;
+		GetModuleInformation(hP, hM, &mod, sizeof(mod));
+		if (ps.Search(0x00,mod.lpBaseOfDll, mod.SizeOfImage, ptr_t)) {
+			SIZE_T t;
+			WriteProcessMemory(hP, (void*) (ptr_t[0]+3), dummy, 3, &t);
+		}
+		break;
+
+	}
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
